1b01a260e6d4ca8671d257e9c91fb09145477eec
tufei <1024522245@qq.com>
2018-02-17 20:55:01 +0800

    移除代码，添加依赖

--- app/src/main/java/com/tufei/architecturedemo/base/mvp/IBaseView.java
+++
@@ -1,8 +0,0 @@
-package com.tufei.architecturedemo.base.mvp;
-
-/**
- * Created by tufei on 2017/6/29.
- */
-public interface IBaseView {
-
-}
--- app/src/main/java/com/tufei/architecturedemo/mvp/Single/Single1.java
+++
@@ -1,9 +0,0 @@
-package com.tufei.architecturedemo.mvp.Single;
-
-/**
- * @author tufei
- * @date 2017/12/7.
- */
-
-public class Single1 {
-}
